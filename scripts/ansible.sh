93f25867c0b2896e25c67cd0acab0f868d817185
kraeml <kraeml@sesamestreet.franken.de>
2018-07-17 13:26:31 +0200

    Create all ubuntu boxes

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -eux
 
 # Install Ansible repository.
-tee "/etc/apt/apt.conf.d/01proxy" > "/dev/null" <<EOF
-Acquire::http { Proxy "http://michl-laptop.fritz.box:3142"; };
-Acquire::https { Proxy "https://"; };
-EOF
+#tee "/etc/apt/apt.conf.d/01proxy" > "/dev/null" <<EOF
+#Acquire::http { Proxy "http://michl-laptop.fritz.box:3142"; };
+#Acquire::https { Proxy "https://"; };
+#EOF
 if ansible --version ; then
 	apt-get -y update
 else
-	apt-get -y update && apt-get -y upgrade
+	apt-get -y update && apt-get -y --force yes upgrade
 	apt-get -y install software-properties-common
 	apt-add-repository ppa:ansible/ansible
 	# Install Ansible.
 	apt-get -y update
 	apt-get -y install ansible
 fi
-apt-get -y full-upgrade
+# apt-get -y --force yes full-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 echo Going reboot
 reboot
 echo Reboot starts
