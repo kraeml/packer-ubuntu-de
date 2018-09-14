3130fa9120ae3859c560a7480ba1f76a81e84605
kraeml <kraeml@sesamestreet.franken.de>
2018-09-14 12:39:31 +0200

    Adding usb-create and refactor rdf-edu script

--- create-usb/create-usb.sh
+++ create-usb/create-usb.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
-BUILD="${PR:="RDF"}"
 
 if [ "${DEBUG:=false}" = "true" ]; then
   set -o xtrace
 fi
-if [ "${SCHUELER:=Schueler}" = "true" ]; then
-  SCHUELER="foo"
-fi
 
-version=${BUILD}-$(cat VERSION)
-small_version=$(cat VERSION)
-BOX_VERSION='*-ubuntu1604-'${version}'.box'
-for i in  $(ls -d /media/michl/PR_*); do
-    mkdir ${i}/builds || true
-    cd ${i}/builds
-    for f in $(ls v*-ubuntu1604-*); do
-        if (echo $f | grep -v ${small_version}); then
-            rm $f
-            #echo löschen
-        fi
-    done
-    cd -
-    rsync -av --progress ./builds/${BOX_VERSION}  ${i}/builds/;
-    rsync -av --progress ./builds/windows_2016_hyperv_virtualbox.box  ${i}/builds/;
-    # rsync -av ./builds/windows_2016_hyperv_virualbox.box  ${i}/builds/;
-    rsync -av --progress ./Vagrantfile ${i}/
-    rsync -av --progress --human-readable --exclude='.git*' \
-      --exclude='.ipynb_checkpoints' \
-      --exclude='.cache' \
-      --exclude='__pycache__' \
-      --exclude='python.itcarlow.ie' \
-      --exclude=${SCHUELER} \
-      --ignore-errors \
-      --delete \
-      ./downloads/ ${i}/downloads/
-    #rsync -av --progress --human-readable \
-    #  --exclude='.vagrant' ka-sa-pr-build ${i}
+#if [ "${SCHUELER:=Schueler}" = "true" ]; then
+#  SCHUELER="foo"
+#fi
+
+for build in $(ls builds/)
+do
+  echo $build
+
+  BUILD_OUT="builds/$(echo $build | sed 's/_/\//g')/box"
+
+
+  version=$(cat builds/${build}/version)
+  for stick in  $(ls -d ${OUTPUT_DIR:=/media/$(whoami)/PR_*}); do
+    #echo $stick
+    mkdir -p ${stick}/${BUILD_OUT} 2>/dev/null || true
+  #    cd ${i}/builds
+  #    for f in $(ls v*-ubuntu1604-*); do
+  #        if (echo $f | grep -v ${small_version}); then
+  #            rm $f
+  #            #echo löschen
+  #        fi
+  #    done
+  #    cd -
+    rsync -av --progress --exclude='.vagrant' --human-readable builds/${build}/  ${stick}/${BUILD_OUT}/;
+  #    rsync -av --progress ./builds/windows_2016_hyperv_virtualbox.box  ${i}/builds/;
+  #    # rsync -av ./builds/windows_2016_hyperv_virualbox.box  ${i}/builds/;
+  #    rsync -av --progress ./Vagrantfile ${i}/
+  #    rsync -av --progress --human-readable --exclude='.git*' \
+  #      --exclude='.ipynb_checkpoints' \
+  #      --exclude='.cache' \
+  #      --exclude='__pycache__' \
+  #      --exclude='python.itcarlow.ie' \
+  #      --exclude=${SCHUELER} \
+  #      --ignore-errors \
+  #      --delete \
+  #      ./downloads/ ${i}/downloads/
+  #    #rsync -av --progress --human-readable \
+  #    #  --exclude='.vagrant' ka-sa-pr-build ${i}
+  done
 done
 sync
