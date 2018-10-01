26ac1e828e5da5771a16f4d8d2ea64ad868970f3
kraeml <kraeml@sesamestreet.franken.de>
2018-10-01 17:02:30 +0200

    USB now only one box, add lokal skript for all box

--- create-usb/create-lokal.sh
+++ create-usb/create-lokal.sh
@@ -4,29 +4,16 @@ if [ "${DEBUG:=false}" = "true" ]; then
   set -o xtrace
 fi
 
-EXCLUDE="de_xdesktop de_base_xdesktop de_devops_xdesktop de_extended_xdesktop de_ina_xdesktop_electronic"
-builds=${BUILDS:=$(ls -d builds/*_de)}
-BUILDS=""
-for i in $builds; do
-  BUILDS=$BUILDS" "$(basename $i)
-done
-builds=$BUILDS
-
 #if [ "${SCHUELER:=Schueler}" = "true" ]; then
 #  SCHUELER="foo"
 #fi
 
-for stick in  $(ls -d ${OUTPUT_DIR:=/media/$(whoami)/PR_*}); do
-  #echo $stick
-  rsync -av --progress --delete --human-readable /home/michl/Dokumente/OwnCloudKNF/Schule/1_Semester/BSA/tuxcademy ${stick}/
-  rsync -av --progress --delete --human-readable downloads  ${stick}/;
-  for build in $builds
+for stick in  $(ls -d ${OUTPUT_DIR:=~/.rdf-builds}); do
+  for build in $(ls builds/)
   do
     echo Copy $build on $stick
-    rm -rf ${stick}/builds 2>/dev/null || true
-    BUILD_OUT="box"
-    cat builds/${build}/version
-    #rm -rf ${stick}/${BUILD_OUT} 2>/dev/null || true
+    BUILD_OUT="builds/$(echo $build | sed 's/_/\//g')/box"
+    version=$(cat builds/${build}/version)
     mkdir -p ${stick}/${BUILD_OUT} 2>/dev/null || true
 #    cd ${i}/builds
 #    for f in $(ls v*-ubuntu1604-*); do
